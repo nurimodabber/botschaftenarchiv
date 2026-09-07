006bbfb8cf553d25090139243322732034a94cc8
nurimodabber <milan.modabber@gmail.com>
2026-09-07 22:27:06 +0200

    Entferne Tags und Empfaenger aus Kacheln in geraeumiger Ansicht

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'bahai-bib-v3.3';
+const CACHE_NAME = 'bahai-bib-v3.4';
 const SHELL_ASSETS = [
   '/',
   '/index.html',
-  '/css/styles.css?v=3.3',
+  '/css/styles.css?v=3.4',
   '/css/styles.css',
   '/manifest.webmanifest',
   '/favicon.ico',
@@ -11,7 +11,7 @@ const SHELL_ASSETS = [
   '/assets/icons/icon-512-maskable.png',
   '/assets/icons/apple-touch-icon.png',
   '/assets/icons/favicon.svg',
-  '/js/app.js?v=3.3',
+  '/js/app.js?v=3.4',
   '/js/app.js',
   '/js/viewer.js',
   '/js/search.js',
