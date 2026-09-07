0c4acb2d1943e58c995ee1428cc37ef4b1a2651c
nurimodabber <milan.modabber@gmail.com>
2026-09-07 21:42:56 +0200

    Align filter button horizontally next to format selection in portrait mode

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'bahai-bib-v2.6';
+const CACHE_NAME = 'bahai-bib-v2.7';
 const SHELL_ASSETS = [
   '/',
   '/index.html',
-  '/css/styles.css?v=2.6',
+  '/css/styles.css?v=2.7',
   '/css/styles.css',
   '/manifest.webmanifest',
   '/favicon.ico',
