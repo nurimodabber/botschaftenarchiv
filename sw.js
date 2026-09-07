dbd640caace15ea2f79afe9c5073c686a7908fdf
nurimodabber <milan.modabber@gmail.com>
2026-09-07 21:28:03 +0200

    Harmonize mobile tab bar styling and active selection colors with landscape dock design system

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'bahai-bib-v2.3';
+const CACHE_NAME = 'bahai-bib-v2.4';
 const SHELL_ASSETS = [
   '/',
   '/index.html',
-  '/css/styles.css?v=2.3',
+  '/css/styles.css?v=2.4',
   '/css/styles.css',
   '/manifest.webmanifest',
   '/favicon.ico',
